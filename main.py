e7af74ff5ed6d3a2d34868d743250cfae5b84f5e
Kieran Dowling <kieran.dowling@ucd.ie>
2021-05-19 23:15:58 +0100

    Geospatial visualisation added

--- main.py
+++ main.py
@@ -59,21 +59,23 @@ f1000_df = pd.DataFrame(f1000)
 source = ColumnDataSource(df)
 p = figure(plot_width=500, plot_height=500, x_axis_label='Market Cap', y_axis_label='Revenue')
 p.circle(x='Market Cap', y='revenue', source=source)
-output_file('bokeh.html')
-show(p)
+#output_file('bokeh.html')
+#show(p)
 
-#, x_range=list(f1000_df.sector.unique())
-
-
-# merging dataframe containing top 10 most profitable companies with GPS data for visualisations
+# merging dataframe containing top 10 most profitable companies with GPS data for geospatial visualisations
 hq_df = pd.DataFrame({ 'company': [	'Berkshire Hathaway',	'Apple',	'Microsoft',	'JPMorgan Chase',	'Alphabet',	'Bank of America',	'Intel',	'Wells Fargo',	'Citigroup',	'Verizon Communications'],
             'HQ_GPS_lat': [	41.2665652155238,	37.3320344033222,	47.6423030655177,	40.7560918430126,	37.4244444307427,	35.2516984332765,	37.3872860733935,	37.7939978437439,	40.7207149964738,	40.7544511859009],
             'HQ_GPS_lng': [	-95.9359591891807,	-122.029665403973,	-122.136943931254,	-73.9755801511926,	-122.083169609495,	-80.8643427511356,	-121.963906989321,	-122.402969302892,	-74.0112209712649,	-73.9848621227913]})
 
-
+# merge
 top10_with_gps = df.merge(hq_df)
-print(top10_with_gps)
 
-
+states = gpd.read_file('data/geo/USA_States_Generalized.shp')
+states.plot(figsize=(10,10), cmap = 'Dark2')
+pins = sns.scatterplot(data=top10_with_gps, x='HQ_GPS_lng', y='HQ_GPS_lat', hue='company')
+plt.xlabel('Latitude')
+plt.ylabel('Longitude')
+plt.title('Headquarters of the top 10 most profitable companies')
+#plt.show()
 
 
